529f4b2b3da72d77559b305bd6c79b68b08f69a6
Electra <contact@electravehicles.com>
2022-12-28 13:10:17 -0500

    setup redux

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,17 @@
 import '../styles/globals.scss'
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistStore } from 'redux-persist';
+import store from "../store";
+
+let persistor = persistStore(store);
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <Component {...pageProps} />
+      </PersistGate>
+    </Provider>
+  )
 }
---
+++ store/index.js
@@ -0,0 +1,26 @@
+import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
+import { configureStore } from "@reduxjs/toolkit";
+import thunk from "redux-thunk";
+import storage from "redux-persist/lib/storage";
+
+import cart from './cartSlice';
+
+const reducers = combineReducers({
+  cart
+});
+
+const config = {
+  key: 'root',
+  storage
+}
+
+const reducer = persistReducer(config, reducers);
+
+const store = configureStore({
+  reducer: reducer,
+  devTools: process.env.NODE_ENV !== 'production',
+  middleware: [thunk]
+});
+
+export default store;
