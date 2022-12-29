a8c9e0b86b3285d34737fb6b340ff9acc75f299c
Electra <contact@electravehicles.com>
2022-12-29 15:29:11 -0500

    create footer

---
+++ components/footer/Copyright.js
@@ -0,0 +1,42 @@
+import Link from "next/link";
+import styles from "./styles.module.scss";
+
+export default function Copyright() {
+  return (
+    <div className={styles.footer__copyright}>
+      <section>©2022 SHOPPAY All Rights Resereved.</section>
+      <section>
+        <ul>
+          {data.map((link) => (
+            <li>
+              <Link href={link.link}>{link.name}</Link>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </div>
+  );
+}
+
+const data = [
+  {
+    name: "Privacy Center",
+    link: "",
+  },
+  {
+    name: "Privacy & Cookie Policy",
+    link: "",
+  },
+  {
+    name: "Manage Cookies",
+    link: "",
+  },
+  {
+    name: "Terms & Conditions",
+    link: "",
+  },
+  {
+    name: "Copyright Notice",
+    link: "",
+  },
+];
---
+++ components/footer/index.js
@@ -0,0 +1,20 @@
+import Links from './Links';
+import Socials from './Socials';
+import NewsLetter from './NewsLetter';
+import Payment from "./Payment";
+import Copyright from "./Copyright";
+import styles from './styles.module.scss';
+
+export default function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.footer__container}>
+        <Links />
+        <Socials />
+        <NewsLetter />
+        <Payment />
+        <Copyright />
+      </div>
+    </footer>
+  )
+}
